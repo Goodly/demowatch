f7214812122388289affa6d9de0504c9d4570838
sidneyle <sidney.h.le@gmail.com>
2018-07-07 15:28:42 -0700

    fixed the TUA parsing function, wrote the table object to a file

--- scripts/TUA_processing.R
+++ scripts/TUA_processing.R
@@ -26,7 +26,7 @@ tua_parser <- function(tua_text) {
   garbage <- c("”", "“")
   
   # split input into list of strings
-  split_tua <- unlist(strsplit(test_tua, '\"'))
+  split_tua <- unlist(strsplit(tua_text, '\"'))
   
   # check and remove trash characters
   garbage_check <- sapply(garbage, grepl, split_tua)
@@ -41,6 +41,8 @@ tua_parser <- function(tua_text) {
 
 
 ###
+metadata_with_tua$TUA <- sapply(metadata_with_tua$TUA, tua_parser)
 
 
-write_csv(metadata_dat, "metadata_table.csv")
+saveRDS(metadata_with_tua, "data/metadata_table.rds")
+
