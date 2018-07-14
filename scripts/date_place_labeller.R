82b2b927f843f38ed01491246fe03d2aa180532e
unknown <jwarnagieris@berkeley.edu>
2018-07-14 13:15:12 -0700

    various changes + made TUA_processing.R"

--- scripts/date_place_labeller.R
+++ scripts/date_place_labeller.R
@@ -9,6 +9,7 @@ setwd(dir)
 ### read in data
 dat <- read_csv("data/textthresher/dfcrowd1dh_task_run.csv")
 metadata_dat <- read_csv("data/textthresher/dfcrowd1dh_task.csv")
+metadata_with_tua <- read_csv('data/metadata_with_tua.csv')
 
 ### group task runs by task ID and choose one, merge in publication date and location
 training_dat <- dat %>% 
@@ -21,10 +22,20 @@ training_dat <- dat %>%
            "info_highlights_0_case_number",
            "info_article_metadata_city",
            "info_article_metadata_date_published"))
-write_csv(training_dat, "data/metadata_table.csv")
+#write_csv(training_dat, "data/metadata_table.csv")
 
 ### source functions from Nick's script
 source("scripts/Day2Dates.R")
+
+#' @description Converts date to numbered weekday
+#' @param date_object Some date object or sequence of date objects
+#' @return Integer representing the date_object's day of the week.
+#' ie: 2018-06-23 is a Saturday, so it's weekday number is 7.
+weekdayNumber <- function(date_object) {
+  day_list <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+                "Saturday")
+  return (match(weekdays(date_object), day_list))
+}
 
 #' @description Based on weekday mentioned in article text as well as publishing date,
 #' returns the YYYY-MM-DD date of the event described in the article
@@ -42,17 +53,29 @@ getEventDate <- function(article_data) {
   return (cbind(article_data, event_date = event_dates))
 }
 
-#' @description Converts date to numbered weekday
-#' @param date_object Some date object or sequence of date objects
-#' @return Integer representing the date_object's day of the week.
-#' ie: 2018-06-23 is a Saturday, so it's weekday number is 7.
-weekdayNumber <- function(date_object) {
-  day_list <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-                "Saturday")
-  return (match(weekdays(date_object), day_list))
+### testing on training_dat
+names(training_dat)[3] <- 'article_text'
+names(training_dat)[6] <- 'date_published'
+
+train_with_event <- getEventDate(training_dat) # returns NA for date published if no weekday found
+
+### testing on metadata_with_tua
+testFunc <- function(article_data) {
+  pdate_weekday_number <- weekdayNumber(article_data$date_published)
+  event_weekday_number <- t(data.frame(lapply(article_data$article_text, return_days)))
+  # for each item in this event_weekday_number:
+  #   if item is NA --> go to second hierarchical step. call return_days on article_text
+  
+  # CHANGE IT TO CALL ON TUA FIRST
+  phase1
+  phase2
+  phase3
+  phase4
+  
+  
+  date_diff <- pdate_weekday_number - event_weekday_number
+  numeric_diff <- ifelse(date_diff > 0, date_diff,  # > 0: date_diff; < 0: add 7; == 0: leave as is
+                         ifelse(date_diff < 0, date_diff + 7, 0))  # NOTE: ifelse is used for vectorizaiton
+  event_dates <- article_data$date_published - numeric_diff
+  return (cbind(article_data, event_date = event_dates))
 }
-
-names(training_dat)[2] <- 'article_text'
-names(training_dat)[4] <- 'date_published'
-
-train_with_event <- getEventDate(training_dat)
