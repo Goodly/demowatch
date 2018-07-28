161c313fbfc446fdf7b15b7038baef250d7cf666
sidneyle <sidney.h.le@berkeley.edu>
2018-07-27 19:42:18 -0700

    Implemented the quiz processor and fixed some dumb bugs

--- scripts/quiz_processing.R
+++ scripts/quiz_processing.R
@@ -25,8 +25,8 @@ grouped_dat <- dat %>%
 grouped_dat$question_number <- sapply(grouped_dat$question_number, 
                                       function(x) {if (x < 10) {
                                         return(paste0(0, x))
-                                        } else {
-                                          return(as.character(x))}})
+                                      } else {
+                                        return(as.character(x))}})
 
 grouped_dat <- grouped_dat %>% mutate(question_number = paste(topic_number, 
                                                               question_number, 
@@ -45,10 +45,12 @@ all_answers <- matrix(0, ncol = length(all_q) + 2,
 all_answers[, 1:2] <- as.matrix(tasks_and_contributors)
 colnames(all_answers) <- col_names
 
-for (i in 1:nrow(tasks_and_contributors)) {
+### formatter function
+
+answer_formatter <- function(tbl_row) {
   ### retrieve task and contributor
-  task <- tasks_and_contributors[[i, 1]]
-  contributor <- tasks_and_contributors[[i, 2]]
+  task <- tasks_and_contributors[[tbl_row, 1]]
+  contributor <- tasks_and_contributors[[tbl_row, 2]]
   
   ### access the answers
   sub_table <- grouped_dat %>%
@@ -56,9 +58,27 @@ for (i in 1:nrow(tasks_and_contributors)) {
   sub_answers <- as.data.frame(t(sub_table[, 3:4]))
   
   ### transform answers tall to wide
-  colnames(sub_answers) <- unlist(sub_answers[1, ])
-  sub_answers <- sub_answers[-1, ]
+  columns <- unlist(sub_answers[1, ])
+  sub_answers <- as.data.frame(sub_answers[c(FALSE, TRUE), ])
+  colnames(sub_answers) <- columns
+
+  ### fill in columns for missing questions
+  missing_q <- setdiff(all_q, columns)
+  missing_ans <- as.data.frame(matrix(0, ncol = length(missing_q)))
+  colnames(missing_ans) <- missing_q
   
-  ### append into container matrix
+  ### bind to answered questions and reorder
+  all_q_ans <- cbind(sub_answers, missing_ans)[, all_q]
+  return(all_q_ans)
+}
+
+all_q_ans_tbl <- answer_formatter(1)
+
+for (i in 2:nrow(tasks_and_contributors)) {
+  ### format the answers
+  print(i)
+  ans_tbl <- answer_formatter(i)
   
+  ### bind to the first row
+  all_q_ans_tbl <- rbind(all_q_ans_tbl, ans_tbl)
 }
