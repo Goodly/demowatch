85dba04aa7a56277c7a6fd92c3646a0e9e9f1d4c
Aaron Ngai <aaronngai@berkeley.edu>
2019-10-31 19:19:38 -0700

    Test toy data for date labelling

--- Canonicalization/tests/test_date_labeller.R
+++ Canonicalization/tests/test_date_labeller.R
@@ -10,6 +10,8 @@ test_that("date labelling works", {
 })
 
 test_that("date labelling works for toy data set", {
-  actual_date_labelled_data <- label_date()
-  # TODO....
+  input_data <- readRDS(file="tests/data/date_labelling_input_data.rds")
+  actual_date_labelled_data <- label_date(input_data)
+  expected_date_labelled_data <- readRDS(file="tests/data/date_labelling_output.rds")
+  expect_equal(actual_date_labelled_data, expected_date_labelled_data)
 })
--- Canonicalization/tests/tua_generator.R
+++ Canonicalization/tests/tua_generator.R
@@ -1,13 +1,18 @@
+### DIRECTORY
+library(readr)
+dir <- readline("Enter the directory for the `df-canonicalization` folder (without quotes):")
+setwd(dir)
+
 task_id <- c(111, 222, 999)
 article_number <- c(23, 125, 732)
 case_number <- c(1, 2, 3)
 event_place <- c("Atlanta", "Little Rock", "Seattle")
-date_published <- c("2011-10-24", "2011-11-05", "2011-12-16")
+date_published <- as.Date(c("2011-10-24", "2011-11-05", "2011-12-16"))
 TUA <- c(c("There was fake blood running in the streets of London Thursday — thanks to an elaborate, 
            and ultimately spectacularly botched stunt by climate change activists.", "Activists with
            Extinction Rebellion had purchased a decommissioned fire truck on eBay in hopes of spraying
            fake blood on the British Treasury building, but most of it wound up flooding the street, 
-           according to reports."), 
+           according to reports."),
          c("Iraq's Prime Minister Adel Abdul Mahdi has called for dialogue after a third consecutive day
            of deadly anti-government protests in the capital, Baghdad, and several southern cities.",
            "In a statement quoted by local media on Thursday, Abdul Mahdi's office said the prime
@@ -34,10 +39,10 @@ info_article_text.x <- c("There was fake blood running in the streets of London 
            said. The man, a technology administrator in the police intelligence unit, launched the attack in
            his office then moved to other parts of the large 19th century building across the street from
            Notre Dame Cathedral.")
-file_name <- c("london.txt", "baghdad.txt", "paris.txt")
+file_name <- c("\"london.txt\"", "\"baghdad.txt\"", "\"paris.txt\"")
 
 my_data <- data.frame(task_id, article_number, case_number, event_place, date_published, TUA, 
                       info_article_text.x, file_name)
 names(my_data) <- c("task_id", "article_number", "case_number", "event_place", "date_published", "TUA", 
                     "info_article_text.x", "file_name")
-saveRDS(my_data, file = "tests/date_labelling_toy_data.rds")
+saveRDS(my_data, file = "tests/data/date_labelling_input_data.rds")
