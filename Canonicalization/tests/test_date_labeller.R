1d3c9b854ef89508adfda28bb57770772a64d502
Aaron Ngai <aaronngai@berkeley.edu>
2019-11-01 17:01:19 -0700

    More robust TUA generator

--- Canonicalization/tests/test_date_labeller.R
+++ Canonicalization/tests/test_date_labeller.R
@@ -1,17 +1,21 @@
 library(testthat)
 context("Testing date-place labeller")
 
-source ("scripts/pipeline.R")
+# source("scripts/pipeline.R")
+source("scripts/date_place_labeller.R")
+setwd("~/Documents/BerkeleySem6/demowatch/Canonicalization")
 
-test_that("date labelling works", {
-  actual_date_labelled_data <- labelled_metadata_tua
-  expected_date_labelled_data <- readRDS(file="tests/data/labelled_metadata_tua.rds")
-  expect_equal(actual_date_labelled_data, expected_date_labelled_data)
-})
+# test_that("date labelling works", {
+  # actual_date_labelled_data <- labelled_metadata_tua
+  # expected_date_labelled_data <- readRDS(file="tests/data/labelled_metadata_tua.rds")
+  # expect_equal(actual_date_labelled_data, expected_date_labelled_data)
+# })
 
-test_that("date labelling works for toy data set", {
+test_that("date labelling works for simple toy data set", {
   input_data <- readRDS(file="tests/data/date_labelling_input_data.rds")
   actual_date_labelled_data <- label_date(input_data)
   expected_date_labelled_data <- readRDS(file="tests/data/date_labelling_output.rds")
   expect_equal(actual_date_labelled_data, expected_date_labelled_data)
 })
+
+# test phases 1, 2, 3
--- Canonicalization/tests/tua_generator.R
+++ Canonicalization/tests/tua_generator.R
@@ -3,12 +3,12 @@ library(readr)
 dir <- readline("Enter the directory for the `df-canonicalization` folder (without quotes):")
 setwd(dir)
 
-task_id <- c(111, 222, 999)
-article_number <- c(23, 125, 732)
-case_number <- c(1, 2, 3)
-event_place <- c("Atlanta", "Little Rock", "Seattle")
+task_id <- as.integer(c(111, 222, 999))
+article_number <- as.integer(c(23, 125, 732))
+case_number <- as.integer(c(1, 2, 3))
+event_place <- as.character(c("Atlanta", "Little Rock", "Seattle"))
 date_published <- as.Date(c("2011-10-24", "2011-11-05", "2011-12-16"))
-TUA <- c(c("There was fake blood running in the streets of London Thursday — thanks to an elaborate, 
+TUA <- I(list(c("There was fake blood running in the streets of London Thursday — thanks to an elaborate, 
            and ultimately spectacularly botched stunt by climate change activists.", "Activists with
            Extinction Rebellion had purchased a decommissioned fire truck on eBay in hopes of spraying
            fake blood on the British Treasury building, but most of it wound up flooding the street, 
@@ -23,8 +23,8 @@ TUA <- c(c("There was fake blood running in the streets of London Thursday — t
            said.", 
            "The man, a technology administrator in the police intelligence unit, launched the attack in
            his office then moved to other parts of the large 19th century building across the street from
-           Notre Dame Cathedral."))
-info_article_text.x <- c("There was fake blood running in the streets of London Thursday — thanks to an elaborate, 
+           Notre Dame Cathedral.")))
+info_article_text.x <- as.character(c("There was fake blood running in the streets of London Thursday — thanks to an elaborate, 
            and ultimately spectacularly botched stunt by climate change activists. Activists with
            Extinction Rebellion had purchased a decommissioned fire truck on eBay in hopes of spraying
            fake blood on the British Treasury building, but most of it wound up flooding the street, 
@@ -38,8 +38,10 @@ info_article_text.x <- c("There was fake blood running in the streets of London 
            stabbing four police colleagues to death before he was shot and killed, French authorities
            said. The man, a technology administrator in the police intelligence unit, launched the attack in
            his office then moved to other parts of the large 19th century building across the street from
-           Notre Dame Cathedral.")
-file_name <- c("\"london.txt\"", "\"baghdad.txt\"", "\"paris.txt\"")
+           Notre Dame Cathedral."))
+file_name <- as.character(c("\"london.txt\"", "\"baghdad.txt\"", "\"paris.txt\""))
+print(length(case_number))
+print(length(event_place))
 
 my_data <- data.frame(task_id, article_number, case_number, event_place, date_published, TUA, 
                       info_article_text.x, file_name)
