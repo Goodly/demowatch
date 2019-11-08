6d35a6189957c450ebc9f4033bb05c22a9bd028c
Aaron Ngai <aaronngai@berkeley.edu>
2019-11-07 18:34:38 -0800

    test_date_labeller prototype

---
+++ Canonicalization/tests/data/dl_gen1.R
@@ -0,0 +1,48 @@
+### DIRECTORY
+library(readr)
+dir <- readline("Enter the directory for the `df-canonicalization` folder (without quotes):")
+setwd(dir)
+
+task_id <- as.integer(c(111, 222, 999))
+article_number <- as.integer(c(23, 125, 732))
+case_number <- as.integer(c(1, 2, 3))
+event_place <- as.character(c("Atlanta", "Little Rock", "Seattle"))
+date_published <- as.Date(c("2011-10-24", "2011-11-05", "2011-12-16"))
+TUA <- I(list(c("There was fake blood running in the streets of London Thursday — thanks to an elaborate, 
+           and ultimately spectacularly botched stunt by climate change activists.", "Activists with
+           Extinction Rebellion had purchased a decommissioned fire truck on eBay in hopes of spraying
+           fake blood on the British Treasury building, but most of it wound up flooding the street, 
+           according to reports."),
+         c("Iraq's Prime Minister Adel Abdul Mahdi has called for dialogue after a third consecutive day
+           of deadly anti-government protests in the capital, Baghdad, and several southern cities.",
+           "In a statement quoted by local media on Thursday, Abdul Mahdi's office said the prime
+           minister was 'continuing contacts' with protesters in a bid to end the political crisis and
+           'return to normal life'."), 
+         c("PARIS —  A civilian employee raged through Paris police headquarters with a knife Thursday,
+           stabbing four police colleagues to death before he was shot and killed, French authorities
+           said.", 
+           "The man, a technology administrator in the police intelligence unit, launched the attack in
+           his office then moved to other parts of the large 19th century building across the street from
+           Notre Dame Cathedral.")))
+info_article_text.x <- as.character(c("There was fake blood running in the streets of London Thursday — thanks to an elaborate, 
+           and ultimately spectacularly botched stunt by climate change activists. Activists with
+           Extinction Rebellion had purchased a decommissioned fire truck on eBay in hopes of spraying
+           fake blood on the British Treasury building, but most of it wound up flooding the street, 
+           according to reports.",
+           "Iraq's Prime Minister Adel Abdul Mahdi has called for dialogue after a third consecutive day
+           of deadly anti-government protests in the capital, Baghdad, and several southern cities. In a
+           statement quoted by local media on Thursday, Abdul Mahdi's office said the prime minister was 
+           'continuing contacts' with protesters in a bid to end the political crisis and
+           'return to normal life'.",
+           "PARIS —  A civilian employee raged through Paris police headquarters with a knife Thursday,
+           stabbing four police colleagues to death before he was shot and killed, French authorities
+           said. The man, a technology administrator in the police intelligence unit, launched the attack in
+           his office then moved to other parts of the large 19th century building across the street from
+           Notre Dame Cathedral."))
+file_name <- as.character(c("\"london.txt\"", "\"baghdad.txt\"", "\"paris.txt\""))
+
+my_data <- data.frame(task_id, article_number, case_number, event_place, date_published, TUA, 
+                      info_article_text.x, file_name)
+names(my_data) <- c("task_id", "article_number", "case_number", "event_place", "date_published", "TUA", 
+                    "info_article_text.x", "file_name")
+saveRDS(my_data, file = "tests/data/dl_input1.rds")
--- Canonicalization/tests/test_date_labeller.R
+++ Canonicalization/tests/test_date_labeller.R
@@ -1,21 +1,56 @@
 library(testthat)
 context("Testing date-place labeller")
 
-# source("scripts/pipeline.R")
-source("scripts/date_place_labeller.R")
-setwd("~/Documents/BerkeleySem6/demowatch/Canonicalization")
+library(readr)
+dir <- readline("Enter the directory for the `df-canonicalization` folder (without quotes):")
+setwd(dir)
 
-# test_that("date labelling works", {
-  # actual_date_labelled_data <- labelled_metadata_tua
-  # expected_date_labelled_data <- readRDS(file="tests/data/labelled_metadata_tua.rds")
-  # expect_equal(actual_date_labelled_data, expected_date_labelled_data)
-# })
+source("scripts/date_place_labeller.R")
+
+if (FALSE) {
+  source("scripts/pipeline.R")
+  test_that("date labelling works", {
+    actual_date_labelled_data <- labelled_metadata_tua
+    expected_date_labelled_data <- readRDS(file="tests/data/labelled_metadata_tua.rds")
+    expect_equal(actual_date_labelled_data, expected_date_labelled_data)
+  })
+}
 
 test_that("date labelling works for simple toy data set", {
-  input_data <- readRDS(file="tests/data/date_labelling_input_data.rds")
+  input_data <- readRDS(file="tests/data/dl_input1.rds")
   actual_date_labelled_data <- label_date(input_data)
-  expected_date_labelled_data <- readRDS(file="tests/data/date_labelling_output.rds")
+  expected_date_labelled_data <- readRDS(file="tests/data/dl_output1.rds")
   expect_equal(actual_date_labelled_data, expected_date_labelled_data)
 })
 
-# test phases 1, 2, 3
+test_that("date labelling phase 1 works", {
+  input_data <- readRDS(file="tests/data/dl_input2.rds")
+  actual_date_labelled_data <- label_date(input_data)
+  expected_date_labelled_data <- readRDS(file="tests/data/dl_output2.rds")
+  expect_equal(actual_date_labelled_data, expected_date_labelled_data)
+})
+
+test_that("date labelling phase 2 works", {
+  input_data <- readRDS(file="tests/data/dl_input3.rds")
+  actual_date_labelled_data <- label_date(input_data)
+  expected_date_labelled_data <- readRDS(file="tests/data/dl_output3.rds")
+  expect_equal(actual_date_labelled_data, expected_date_labelled_data)
+})
+
+test_that("date labelling phase 3 works", {
+  input_data <- readRDS(file="tests/data/dl_input4.rds")
+  actual_date_labelled_data <- label_date(input_data)
+  expected_date_labelled_data <- readRDS(file="tests/data/dl_output4.rds")
+  expect_equal(actual_date_labelled_data, expected_date_labelled_data)
+})
+
+test_that("date labelling works with null values", {
+  input_data <- readRDS(file="tests/data/dl_input5.rds")
+  actual_date_labelled_data <- label_date(input_data)
+  expected_date_labelled_data <- readRDS(file="tests/data/dl_output5.rds")
+  expect_equal(actual_date_labelled_data, expected_date_labelled_data)
+})
+
+# input_data <- readRDS(file="tests/data/dl_input5.rds")
+# output <- label_date(input_data)
+# saveRDS(output, file = "tests/data/dl_output5.rds")
