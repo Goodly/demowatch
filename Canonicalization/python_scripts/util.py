2e9511f53ffbd632715cf418773f813309f15ca0
Will Furtado <willfurtado@berkeley.edu>
2020-11-14 10:45:16 -0800

    Implementation of gzip converter

--- Canonicalization/python_scripts/util.py
+++ Canonicalization/python_scripts/util.py
@@ -8,8 +8,8 @@ def convert_gzip(path):
     str path: path to folder to convert
     """
     with gzip.open(path, 'rb') as f_in:
-    with open(path[:-3], 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
+        with open(path[:-3], 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
     
 
 def day_from_date(date_string):
